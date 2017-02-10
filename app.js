b9453b19d22a7dff1e27db8e2184f3c371ff6cb9
Dave <dave@Daves-MacBook-Pro.local>
2017-02-10 13:19:38 +0000

    move Google Maps API code server-side, to be inserted into DOM on startup

--- app.js
+++ app.js
@@ -44,37 +44,36 @@ var ViewModel = function() {
 
 var GoogleMapsVM = function() {
 	var self = this;
-	// this.gMapsUrl = "https://maps.googleapis.com/maps/api/js?key=";
-	// gMapsUrl += "AIzaSyC1TZRJK7_lN0GZhEDga7vPu8pm_v7qWIQ&callback=initMap";
-	// $.ajax(gMapsUrl).done(function() {
-	// 	console.log("initMap Called Successfully");
-	// 	map = new google.maps.Map( $('#map'), {
- //          zoom: 2,
- //          center: new google.maps.LatLng(2.8,-187.3),
- //          mapTypeId: 'terrain'
-	// 	});
-	// });
-	// Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        // This example uses a local copy of the GeoJSON stored at
-        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
-      
 
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
+	this.init = function() {
+		var mapsCallback = $("<script>");
+		mapsCallback.html( this.mapsCallback() );
+
+		var gMapsUrl = "https://maps.googleapis.com/maps/api/js?" +
+			"key=AIzaSyC1TZRJK7_lN0GZhEDga7vPu8pm_v7qWIQ&callback=initMap";
+		var mapsScript = $("<script>", {
+			"src": gMapsUrl,
+		});
+		mapsScript[0].setAttribute("async", "");
+		mapsScript[0].setAttribute("defer", "");
+		$('body').append(mapsCallback);
+		$('body').append(mapsScript);
+	};
+
+	this.mapsCallback = function() {
+		var callbackString = "";
+		callbackString += "var map;";
+		callbackString += "function initMap() {";
+		callbackString += "	map = new google.maps.Map(document.getElementById('map'), {";
+		callbackString += " zoom: 2,";
+		callbackString += "	center: new google.maps.LatLng(2.8,-187.3),";
+		callbackString += "	mapTypeId: 'terrain'";
+		callbackString += "	} ); }";
+		return callbackString;
+	};
+
+	this.init();
 };
 
-// GoogleMapsVM();
+GoogleMapsVM();
 ko.applyBindings( new ViewModel() );
